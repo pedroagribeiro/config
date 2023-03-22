9f6905b96139cefcaf68e436114eb2796abecd3a
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2023-03-22 23:22:15 +0000

    [i3]: Adds the i3 configuration files

---
+++ i3/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+install_package i3
+
+symlink "$HOME/config/i3/config" "$HOME/.config/i3/config"
+
