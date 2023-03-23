36e648ba71c7229d57a8ff4359badf4eb7417bcc
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2023-03-23 00:49:55 +0000

    [podman]: Adds podman installation script

---
+++ podman/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#set -Eeuo pipefail
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+install_package podman
+
+execute "podman machine init" "Initializing the Podman machine..."
+
+execute "podman machine start" "Starting the Podman machine..."
+
