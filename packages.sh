578e70c1744274b6bb70bf31818cec279e55a6d4
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2023-03-23 01:05:08 +0000

    [install.sh]: Adds installation scripts that definitely needs tunning

---
+++ packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export BREW=(
+  ansible
+  direnv
+  git
+  git-lfs
+  maven
+  minikube
+  ncurses
+  neofetch
+  neovim
+  speedtest-cli
+  telnet
+  wget
+  yabai
+)
+
+export REPO=()
+
+export AUR=()
