149341c13b5831bd78ccb88a9e309fa02909f633
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2023-03-27 21:11:06 +0100

    [nvim]: Adds neovim configuration files and installaton scripts

---
+++ nvim/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+# Neovim releases
+# github.com/neovim/neovim/releases
+function custom_neovim_install {
+	wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.tar.gz
+	tar xvzf nvim-linux64.tar.gz
+	mv nvim-linux64 $HOME/.local/bin/.
+	rm nvim-linux64.tar.gz
+	symlink "$HOME/.local/bin/nvim-linux64/bin/nvim" "$HOME/.local/bin/nvim"
+}
+
+function install_packer {
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+	~/.local/share/nvim/site/pack/packer/start/packer.nvim
+}
+
+
+custom_neovim_install
+
+# symlink "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+
